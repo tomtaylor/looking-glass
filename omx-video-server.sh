9e93448b892922153685c82c72cd3f0f270c205d
Tom Taylor <tom@tomtaylor.co.uk>
2013-05-06 21:12:35 +0100

    Send config at interval.

--- omx-video-server.sh
+++ omx-video-server.sh
@@ -19,4 +19,5 @@ gst-launch-1.0 -v v4l2src device=/dev/video0 \
   ! "video/x-raw,width=${WIDTH},height=${HEIGHT},framerate=${FPS}/1" \
   ! clockoverlay valignment=bottom halignment=left \
   ! textoverlay valignment=bottom halignment=right text="${HOSTNAME}" \
-  ! omxh264enc ! rtph264pay ! udpsink host=${HOST} port=${PORT} sync=false
+  ! omxh264enc ! rtph264pay config-interval=1 \
+  ! udpsink host=${HOST} port=${PORT} sync=false
