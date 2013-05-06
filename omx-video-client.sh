d7c352fff34a71fafbf32dcc3315beca809742a1
Tom Taylor <tom@tomtaylor.co.uk>
2013-05-06 20:53:41 +0100

    Initial import.

---
+++ omx-video-client.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -ue
+DEFAULT_PORT=5000
+: ${PORT=$DEFAULT_PORT}
+
+echo "Receiving video on port ${PORT}"
+
+gst-launch-1.0 -v udpsrc port=$PORT ! "application/x-rtp, payload=127" ! rtph264depay ! h264parse ! omxh264dec ! eglglessink sync=false
---
+++ omx-video-server.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -ue
+DEFAULT_HOST="127.0.0.1"
+DEFAULT_PORT=5000
+DEFAULT_WIDTH=640
+DEFAULT_HEIGHT=360
+DEFAULT_FPS=30
+
+: ${HOST=$DEFAULT_HOST}
+: ${PORT=$DEFAULT_PORT}
+: ${WIDTH=$DEFAULT_WIDTH}
+: ${HEIGHT=$DEFAULT_HEIGHT}
+: ${FPS=$DEFAULT_FPS}
+
+echo "Serving /dev/video0 at ${WIDTH}x${HEIGHT}px at ${FPS}fps to ${HOST}:${PORT}"
+
+gst-launch-1.0 -v v4l2src device=/dev/video0 ! "video/x-raw,width=${WIDTH},height=${HEIGHT},framerate=${FPS}/1" !  omxh264enc ! rtph264pay ! udpsink host=${HOST} port=${PORT} sync=false
