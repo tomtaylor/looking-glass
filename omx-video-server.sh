657b584a3ae09ca3f6b3538f89a23206666fb9f0
Tom Taylor <tom@tomtaylor.co.uk>
2013-05-06 21:22:17 +0100

    Make overlay text more readable.

--- omx-video-server.sh
+++ omx-video-server.sh
@@ -18,7 +18,7 @@ HOSTNAME=`hostname -f`
 set -x
 gst-launch-1.0 -v v4l2src device=/dev/video0 \
   ! "video/x-raw,width=${WIDTH},height=${HEIGHT},framerate=${FPS}/1" \
-  ! clockoverlay valignment=bottom halignment=left \
-  ! textoverlay valignment=bottom halignment=right text="${HOSTNAME}" \
+  ! clockoverlay shaded-background=true valignment=bottom halignment=left \
+  ! textoverlay shaded-background=true valignment=bottom halignment=right text="${HOSTNAME}" \
   ! omxh264enc ! rtph264pay config-interval=1 \
   ! udpsink host=${HOST} port=${PORT} sync=false
