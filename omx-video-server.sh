b1468a1f6a09edef9cc763e3b0b076ccc8448566
Tom Taylor <tom@tomtaylor.co.uk>
2013-05-07 08:09:36 +0100

    Include IP address in overlay.

--- omx-video-server.sh
+++ omx-video-server.sh
@@ -13,12 +13,13 @@ DEFAULT_FPS=30
 
 echo "Serving /dev/video0 at ${WIDTH}x${HEIGHT}px at ${FPS}fps to ${HOST}:${PORT}"
 
+IP=`ifconfig eth0 | grep "inet addr" | cut -d: -f2 | cut -d' ' -f1`
 HOSTNAME=`hostname -f`
 
 set -x
 gst-launch-1.0 -v v4l2src device=/dev/video0 \
   ! "video/x-raw,width=${WIDTH},height=${HEIGHT},framerate=${FPS}/1" \
   ! clockoverlay shaded-background=true valignment=bottom halignment=left \
-  ! textoverlay shaded-background=true valignment=bottom halignment=right text="${HOSTNAME}" \
+  ! textoverlay shaded-background=true valignment=bottom halignment=right text="${HOSTNAME} (${IP})" \
   ! omxh264enc ! rtph264pay config-interval=1 \
   ! udpsink host=${HOST} port=${PORT} sync=false
