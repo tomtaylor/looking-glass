460b1052dcdb8413071a0ceae0d9b81e42448ad1
Tom Taylor <tom@tomtaylor.co.uk>
2013-05-08 20:23:44 +0100

    Default text to hostname.

--- omx-video-server.sh
+++ omx-video-server.sh
@@ -4,22 +4,21 @@ DEFAULT_PORT=5000
 DEFAULT_WIDTH=640
 DEFAULT_HEIGHT=480
 DEFAULT_FPS=30
+DEFAULT_TEXT=`hostname -f`
 
 : ${HOST=$DEFAULT_HOST}
 : ${PORT=$DEFAULT_PORT}
 : ${WIDTH=$DEFAULT_WIDTH}
 : ${HEIGHT=$DEFAULT_HEIGHT}
 : ${FPS=$DEFAULT_FPS}
+: ${TEXT=$DEFAULT_TEXT}
 
 echo "Serving /dev/video0 at ${WIDTH}x${HEIGHT}px at ${FPS}fps to ${HOST}:${PORT}"
-
-IP=`ifconfig eth0 | grep "inet addr" | cut -d: -f2 | cut -d' ' -f1`
-HOSTNAME=`hostname -f`
 
 set -x
 exec gst-launch-1.0 -v v4l2src device=/dev/video0 \
   ! "video/x-raw,width=${WIDTH},height=${HEIGHT},framerate=${FPS}/1" \
   ! clockoverlay shaded-background=true valignment=bottom halignment=left \
-  ! textoverlay shaded-background=true valignment=bottom halignment=right text="${HOSTNAME} (${IP})" \
+  ! textoverlay shaded-background=true valignment=bottom halignment=right text="${TEXT}" \
   ! omxh264enc ! rtph264pay config-interval=1 \
   ! udpsink host=${HOST} port=${PORT} sync=false
