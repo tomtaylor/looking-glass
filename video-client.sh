3907a6f804efd44586c3386719d5683affc6169c
Tom Taylor <tom@tomtaylor.co.uk>
2013-05-08 21:18:03 +0100

    Allow configurable sink in client.

--- video-client.sh
+++ video-client.sh
@@ -1,11 +1,13 @@
 #!/bin/bash -ue
 DEFAULT_PORT=5000
 DEFAULT_DECODER="omxh264dec"
+DEFAULT_SINK="eglglessink"
 
 : ${PORT=$DEFAULT_PORT}
 : ${DECODER=$DEFAULT_DECODER}
+: ${SINK=$DEFAULT_SINK}
 
 echo "Receiving video on port ${PORT}"
 
 exec gst-launch-1.0 -v udpsrc port=$PORT ! "application/x-rtp, payload=127" \
-  ! rtph264depay ! h264parse ! ${DECODER} ! eglglessink sync=false
+  ! rtph264depay ! h264parse ! ${DECODER} ! ${SINK} sync=false
